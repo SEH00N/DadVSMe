36298d074cfec3b7a2073a6e58d3a9a50c3f82b8
SEH00N <06psh0501@gmail.com>
2025-07-19 00:33:02 +0900

    [Package] Import Module

---
+++ DadVSMeClient/Assets/01.Scripts/Module/H00N/Runtime/UI/AnchoredRect.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace H00N.UI
+{
+    public struct AnchoredRect
+    {
+        public Vector2 position;
+        public Vector2 size;
+        public Vector2 pivot;
+    }
+}
