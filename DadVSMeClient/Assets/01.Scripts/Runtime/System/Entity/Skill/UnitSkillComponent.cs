828c916ee374b8e03f027e6b051010d527df597d
JUNSUNG06 <forest2847@gmail.com>
2025-08-10 03:43:18 +0900

    [Skill] Generate unit skill base classes

---
+++ DadVSMeClient/Assets/01.Scripts/Runtime/System/Entity/Skill/UnitSkillComponent.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DadVSMe
+{
+    public class UnitSkillComponent : MonoBehaviour
+    {
+        private Dictionary<Type, UnitSkill> skillContainer;
+
+        public void RegistSkill<T>(T skill) where T : UnitSkill
+        {
+            Type skillType = skill.GetType();
+
+            if (skillContainer.ContainsKey(skillType))
+            {
+                skillContainer[skillType].LevelUp();
+            }
+            else
+            {
+                skillContainer.Add(skillType, skill);
+                skill.OnRegist(gameObject);
+            }
+        }
+
+        public void UnregistSkill(Type skillType)
+        {
+            if (skillContainer.ContainsKey(skillType))
+            {
+                skillContainer[skillType].OnUnregist();
+                skillContainer.Remove(skillType);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
