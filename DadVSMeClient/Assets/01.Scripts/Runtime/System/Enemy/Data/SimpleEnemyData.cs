bf242e053e236de628cc344eb6947aa047cec9a5
SEH00N <06psh0501@gmail.com>
2025-08-20 01:18:05 +0900

    [Enemy] Butt Attack

---
+++ DadVSMeClient/Assets/01.Scripts/Runtime/System/Enemy/Data/SimpleEnemyData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace DadVSMe.Enemies
+{
+    [CreateAssetMenu(menuName = "DadVSMe/EntityData/SimpleEnemyData")]
+    public class SimpleEnemyData : KidEnemyData
+    {
+    }
+}
