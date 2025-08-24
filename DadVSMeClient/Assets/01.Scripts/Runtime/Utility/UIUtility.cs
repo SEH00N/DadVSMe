c1d9d2e7bdf72ea8a1474d1e111fc04dda0555ab
장하원 <98999600+janghawon@users.noreply.github.com>
2025-08-24 19:46:10 +0900

    [Background] Edit Layers

--- DadVSMeClient/Assets/01.Scripts/Runtime/Utility/UIUtility.cs
+++ DadVSMeClient/Assets/01.Scripts/Runtime/Utility/UIUtility.cs
@@ -16,14 +16,14 @@ namespace DadVSMe
 
             GameInstance.FadeImage.DOKill();
             GameInstance.FadeImage.color = new Color(GameInstance.FadeImage.color.r, GameInstance.FadeImage.color.g, GameInstance.FadeImage.color.b, 0f);
-            await GameInstance.FadeImage.DOFade(1f, duration).SetEase(Ease.OutCubic);
+            //await GameInstance.FadeImage.DOFade(1f, duration).SetEase(Ease.OutCubic);
         }
 
         public static async UniTask FadeOutAsync(float duration = GameDefine.DEFAULT_FADE_DURATION)
         {
             GameInstance.FadeImage.DOKill();
             GameInstance.FadeImage.color = new Color(GameInstance.FadeImage.color.r, GameInstance.FadeImage.color.g, GameInstance.FadeImage.color.b, 1f);
-            await GameInstance.FadeImage.DOFade(0f, duration).SetEase(Ease.InCubic);
+            //await GameInstance.FadeImage.DOFade(0f, duration).SetEase(Ease.InCubic);
 
             GameInstance.FadeImage.raycastTarget = false;
             InputBlock.Release(INPUT_BLOCK_KEY);
