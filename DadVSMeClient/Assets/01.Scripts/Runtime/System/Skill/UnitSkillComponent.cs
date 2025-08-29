8a4c57352d989d1b37dad1f904bf422111d3e08e
SEH00N <06psh0501@gmail.com>
2025-08-30 01:45:55 +0900

    [ETC]

--- DadVSMeClient/Assets/01.Scripts/Runtime/System/Skill/UnitSkillComponent.cs
+++ DadVSMeClient/Assets/01.Scripts/Runtime/System/Skill/UnitSkillComponent.cs
@@ -18,10 +18,10 @@ namespace DadVSMe
         {
             skillContainer = new();
 
-            RegistSkill(SkillType.ItemMagnet);
+            // RegistSkill(SkillType.ItemMagnet);
             // RegistSkill(SkillType.GuidedOrb);
             // RegistSkill(SkillType.AttackBlast);
-            RegistSkill(SkillType.FirePunch);
+            // RegistSkill(SkillType.FirePunch);
         }
 
         public void RegistSkill(SkillType skillType)
