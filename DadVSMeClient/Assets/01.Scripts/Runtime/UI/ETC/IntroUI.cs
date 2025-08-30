fdeb4ab56c78fe1a57dd35341a63cad35d3aa39b
SEH00N <06psh0501@gmail.com>
2025-08-30 14:41:26 +0900

    [UI] Intro Skip Button

--- DadVSMeClient/Assets/01.Scripts/Runtime/UI/ETC/IntroUI.cs
+++ DadVSMeClient/Assets/01.Scripts/Runtime/UI/ETC/IntroUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using H00N.Resources;
 using H00N.Resources.Addressables;
@@ -17,45 +18,60 @@ namespace DadVSMe.UI
         [SerializeField] VideoPlayer videoPlayer = null;
         [SerializeField] AddressableAsset<VideoClip> videoClipAsset = null;
 
+        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
         public async void OnTouchStartButton()
         {
             try 
-            {
+            {                
+                cancellationTokenSource?.Cancel();
+                cancellationTokenSource?.Dispose();
+                cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(destroyCancellationToken);
+
                 // Holding
-                await UniTask.Delay(TimeSpan.FromSeconds(DELAY_TIME));
+                await UniTask.Delay(TimeSpan.FromSeconds(DELAY_TIME), cancellationToken: cancellationTokenSource.Token);
 
                 // FadeIn
-                await DOFade.FadeInAsync();
+                await DOFade.FadeInAsync().AttachExternalCancellation(cancellationTokenSource.Token);
 
                 // Load video clip
-                await videoClipAsset.InitializeAsync();
+                await videoClipAsset.InitializeAsync().AttachExternalCancellation(cancellationTokenSource.Token);
 
                 cutscenePanelObject.SetActive(true);
                 videoPlayer.clip = videoClipAsset;
                 videoPlayer.Prepare();
-                await UniTask.WaitUntil(() => videoPlayer.isPrepared);
+                await UniTask.WaitUntil(() => videoPlayer.isPrepared, cancellationToken: cancellationTokenSource.Token);
 
                 // FadeOut
-                await DOFade.FadeOutAsync();
+                await DOFade.FadeOutAsync().AttachExternalCancellation(cancellationTokenSource.Token);
 
                 // Play video
                 videoPlayer.Play();
 
                 // Wait for video to finish
-                await UniTask.Delay(TimeSpan.FromSeconds(videoPlayer.length + VIDEO_WAIT_TIME), cancellationToken: destroyCancellationToken);
+                await UniTask.Delay(TimeSpan.FromSeconds(videoPlayer.length + VIDEO_WAIT_TIME), cancellationToken: cancellationTokenSource.Token);
 
-                ResourceManager.ReleaseResource(videoClipAsset);
+                if(cancellationTokenSource.IsCancellationRequested)
+                    return;
 
                 // Load Ongoing Scene
                 await SceneManager.TryLoadSceneAsync(GameDefine.TITLE_SCENE_NAME, LoadSceneMode.Single);
             }
             catch(OperationCanceledException) { }
+            finally {
+                ResourceManager.ReleaseResource(videoClipAsset);
+                cancellationTokenSource?.Dispose();
+                cancellationTokenSource = null;
+            }
         }
 
         public async void OnTouchSkipButton()
         {
+            cancellationTokenSource?.Cancel();
+            cancellationTokenSource?.Dispose();
+            cancellationTokenSource = null;
+
             videoPlayer.time = videoPlayer.length + VIDEO_WAIT_TIME;
-            ResourceManager.ReleaseResource(videoClipAsset);
             await SceneManager.TryLoadSceneAsync(GameDefine.TITLE_SCENE_NAME, LoadSceneMode.Single);
         }
     }
