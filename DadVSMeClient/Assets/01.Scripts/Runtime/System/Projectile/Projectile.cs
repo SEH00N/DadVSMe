77559a71dcf1abbc8c031b77c783a9c204dc98be
SEH00N <06psh0501@gmail.com>
2025-08-31 04:13:33 +0900

    [Unit] Animal Resizing & Projectile Trail

--- DadVSMeClient/Assets/01.Scripts/Runtime/System/Projectile/Projectile.cs
+++ DadVSMeClient/Assets/01.Scripts/Runtime/System/Projectile/Projectile.cs
@@ -1,16 +1,20 @@
 using DadVSMe.Entities;
 using H00N.Resources.Pools;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace DadVSMe
 {
     public abstract class Projectile : PoolReference
     {
+        [SerializeField] UnityEvent onInitializedEvent = null;
+
         protected Unit owner = null;
 
         public virtual void Initialize(Unit owner, Vector2 targetPosition)
         {
             this.owner = owner;
+            onInitializedEvent?.Invoke();
         }
     }
 }
