27cdc92fb0d3e187ae3506947b2eb3a7927bb4f4
SEH00N <06psh0501@gmail.com>
2025-08-31 16:36:11 +0900

    [Game] Ending Direction

---
+++ DadVSMeClient/Assets/01.Scripts/Runtime/UI/ETC/EndingUI.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DadVSMe.UI
+{
+    public class EndingUI : MonoBehaviour
+    {
+        [SerializeField] List<Image> imageList = null;
+        [SerializeField] RectTransform creditTransform = null;
+
+        private void Start()
+        {
+            // start animation
+            PlayAnimation();
+        }
+
+        private async void PlayAnimation()
+        {
+            foreach (Image image in imageList)
+            {
+                image.gameObject.SetActive(true);
+                image.color = Color.white;
+            }
+            
+            await UniTask.WaitForSeconds(1f);
+
+            foreach (Image image in imageList)
+            {
+                await UniTask.WaitForSeconds(3f);
+                await image.DOFade(0f, 1.5f).SetEase(Ease.OutCubic);
+            }
+
+            await UniTask.WaitForSeconds(1f);
+
+            // await creditTransform.DOAnchorPosY(1060f, 2f).SetEase(Ease.InOutSine);
+            // await UniTask.WaitForSeconds(3f);
+            // await creditTransform.DOAnchorPosY(1935f, 2f).SetEase(Ease.InOutSine);
+            // await UniTask.WaitForSeconds(3f);
+            // await creditTransform.DOAnchorPosY(2855, 2f).SetEase(Ease.InOutSine);
+            // await UniTask.WaitForSeconds(3f);
+            // await creditTransform.DOAnchorPosY(3870, 2f).SetEase(Ease.InOutSine);
+            // await UniTask.WaitForSeconds(3f);
+            // await creditTransform.DOAnchorPosY(5020, 2f).SetEase(Ease.InOutSine);
+            // await UniTask.WaitForSeconds(3f);
+            // await creditTransform.DOAnchorPosY(6100, 2f).SetEase(Ease.InOutSine);
+
+            await creditTransform.DOAnchorPosY(100, 3f).SetEase(Ease.InCubic);
+            await creditTransform.DOAnchorPosY(6100, 50f).SetEase(Ease.Linear);
+        }
+    }
+}
