271cd24083fa6eb0a75ca4e8b8bdc7d2a6473f5f
SEH00N <06psh0501@gmail.com>
2025-08-31 16:59:16 +0900

    [UI] Go Text Sound Effect

--- DadVSMeClient/Assets/01.Scripts/Runtime/UI/HUD/HUDUI.cs
+++ DadVSMeClient/Assets/01.Scripts/Runtime/UI/HUD/HUDUI.cs
@@ -2,6 +2,7 @@ using Cysharp.Threading.Tasks;
 using DadVSMe.GameCycles;
 using DadVSMe.Players;
 using DadVSMe.UI.Skills;
+using H00N.Resources.Addressables;
 using UnityEngine;
 
 namespace DadVSMe.UI.HUD
@@ -14,6 +15,7 @@ namespace DadVSMe.UI.HUD
         [SerializeField] SkillInfoUI skillInfoUI = null;
         [SerializeField] GameProgressUI gameProgressUI = null;
         [SerializeField] AnimationUI goPopupUI = null;
+        [SerializeField] AddressableAsset<AudioClip> goPopupSound = null;
 
         public void Initialize()
         {
@@ -24,12 +26,14 @@ namespace DadVSMe.UI.HUD
             expBarUI.Initialize(player);
             skillInfoUI.Initialize(player.GetComponent<UnitSkillComponent>());
             gameProgressUI.Initialize(gameCycle.StartLine, gameCycle.EndLine, gameCycle.Deadline.transform, player.transform);
+            goPopupSound.InitializeAsync().Forget();
             goPopupUI.gameObject.SetActive(false);
         }
 
         public void ActiveGoPopupUI()
         {
             goPopupUI.gameObject.SetActive(true);
+            _ = new PlaySound(goPopupSound);
         }
     }
 }
