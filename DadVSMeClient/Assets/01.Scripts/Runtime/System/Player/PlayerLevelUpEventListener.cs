d33533b5ba321020cc9504429035301862976c2f
SEH00N <06psh0501@gmail.com>
2025-09-05 01:41:29 +0900

    [etc]

--- DadVSMeClient/Assets/01.Scripts/Runtime/System/Player/PlayerLevelUpEventListener.cs
+++ DadVSMeClient/Assets/01.Scripts/Runtime/System/Player/PlayerLevelUpEventListener.cs
@@ -23,11 +23,11 @@ namespace DadVSMe.Players
             levelUpSound.InitializeAsync().Forget();
         }
 
-        private void Update()
-        {
-            if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.A))
-                OnLevelUp(0);
-        }
+        // private void Update()
+        // {
+        //     if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.A))
+        //         OnLevelUp(0);
+        // }
 
         public async void OnLevelUp(int _)
         {
