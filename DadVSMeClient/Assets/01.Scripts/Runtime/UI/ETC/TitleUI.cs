4ab35206ac202db986e6b567e3bbed47d3ebe268
SEH00N <06psh0501@gmail.com>
2025-09-04 22:14:44 +0900

    [UI] How To Play

--- DadVSMeClient/Assets/01.Scripts/Runtime/UI/ETC/TitleUI.cs
+++ DadVSMeClient/Assets/01.Scripts/Runtime/UI/ETC/TitleUI.cs
@@ -4,6 +4,7 @@ using H00N.Resources.Addressables;
 using DadVSMe.UI.Setting;
 using H00N.Resources.Pools;
 using System.Collections.Generic;
+using DG.Tweening;
 
 namespace DadVSMe.UI
 {
@@ -12,6 +13,13 @@ namespace DadVSMe.UI
         [SerializeField] AddressableAsset<SettingPopupUI> settingPopupUIPrefab = null;
         [SerializeField] List<AddressableAsset<AudioClip>> transitionSounds = null;
         [SerializeField] AddressableAsset<AudioClip> clickSound = null;
+
+        [Space(10f)]
+        [SerializeField] GameObject howToPlayPanelObject = null;
+        [SerializeField] GameObject howToPlayContentsObject = null;
+        [SerializeField] CanvasGroup howToPlayPanelCanvasGroup = null;
+
+        private bool isTweening = false;
 
         private void Awake()
         {
@@ -38,7 +46,31 @@ namespace DadVSMe.UI
 
         public void OnTouchHowToPlayButton()
         {
+            if(isTweening)
+                return;
+
+            isTweening = true;
+
             _ = new PlaySound(clickSound);
+            howToPlayPanelObject.SetActive(true);
+            howToPlayContentsObject.transform.localScale = Vector3.one;
+            howToPlayPanelCanvasGroup.alpha = 0f;
+            howToPlayPanelCanvasGroup.DOFade(1f, 0.5f).SetEase(Ease.OutCubic).OnComplete(() => {
+                isTweening = false;
+            });
+        }
+
+        public void OnTouchCloseHowToPlayButton()
+        {
+            if(isTweening)
+                return;
+
+            isTweening = true;
+
+            howToPlayContentsObject.transform.DOScale(0f, 0.2f).SetEase(Ease.OutCubic).OnComplete(() => {
+                howToPlayPanelObject.SetActive(false);
+                isTweening = false;
+            });
         }
 
         public void PlaySound(int index)
