cdba7d56e57f4da79fa5e782ba08f2d960944f9d
SEH00N <06psh0501@gmail.com>
2025-09-05 09:06:48 +0900

    etc

--- DadVSMeClient/Assets/01.Scripts/Runtime/UI/ETC/IntroUI.cs
+++ DadVSMeClient/Assets/01.Scripts/Runtime/UI/ETC/IntroUI.cs
@@ -29,61 +29,39 @@ namespace DadVSMe.UI
                 cancellationTokenSource?.Dispose();
                 cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(destroyCancellationToken);
 
-                Debug.Log("1111111");
-
                 // Holding
                 await UniTask.Delay(TimeSpan.FromSeconds(DELAY_TIME), cancellationToken: cancellationTokenSource.Token);
-
-                Debug.Log("2222222");
 
                 // FadeIn
                 await DOFade.FadeInAsync().AttachExternalCancellation(cancellationTokenSource.Token);
 
-                Debug.Log("3333333");
-
                 // Load video clip
                 await bgmAudioLibrary.InitializeAsync().AttachExternalCancellation(cancellationTokenSource.Token);
 
-                Debug.Log("4444444");
-
                 cutscenePanelObject.SetActive(true);
                 videoHolderObject.SetActive(false);
-
-                Debug.Log("5555555");
 
                 // videoPlayer.clip = videoClipAsset;
                 videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "IntroVideo.mp4");
                 videoPlayer.Prepare();
                 await UniTask.WaitUntil(() => videoPlayer.isPrepared, cancellationToken: cancellationTokenSource.Token);
 
-                Debug.Log("6666666");
-
                 // FadeOut
                 await DOFade.FadeOutAsync().AttachExternalCancellation(cancellationTokenSource.Token);
-
-                Debug.Log("7777777");
 
                 // Play video
                 videoHolderObject.SetActive(true);
                 videoPlayer.Play();
                 AudioManager.Instance.PlayBGM(bgmAudioLibrary, loadCache: false);
 
-                Debug.Log("8888888");
-
                 // Wait for video to finish
                 await UniTask.Delay(TimeSpan.FromSeconds(WAIT_TIME), cancellationToken: cancellationTokenSource.Token);
-
-                Debug.Log("9999999");
 
                 if(cancellationTokenSource.IsCancellationRequested)
                     return;
 
-                Debug.Log("10101010");
-
                 // Load Ongoing Scene
                 await SceneManager.TryLoadSceneAsync(GameDefine.TITLE_SCENE_NAME, LoadSceneMode.Single);
-
-                Debug.Log("11111111");
             }
             catch(OperationCanceledException) { }
             finally {
